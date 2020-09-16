50e155afba2ebf8519dd2dab1d1ff40ab1e277c8
burcj3622 <burcj3622@wrdsb.ca>
2020-09-16 13:40:48 +0000

    pizza

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * pizza
+ * @author jason
  */
 public class Main {
 
@@ -9,7 +10,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // asks user for input
+    System.out.println("What is the size of the pizza (in cm):");
+
+    //user input
+    int number = input.nextInt();
+    
+    //prices
+    double ingredients = (number * 0.5);
+    double labor = (0.75);
+    double power = (0.99);
+    double subtotal = (ingredients+labor+power);
+    double taxes = (subtotal * .13);
+    double total = ( subtotal + taxes);
+   
+   // the total prices 
+  System.out.println("Subtotal:" + "$"+ subtotal);
+  System.out.println("Taxes:" + "$" + taxes);
+  System.out.println("Total:" + "$" + total);
+   
+   
+   
+   // if statments 
+    if (number <= 20){
+       System.out.println("We are going to make you a cute little pizza!");
+    }
+    else if (number <= 40){
+      System.out.println("This will be delicious!");
+    }
+    else if (number >= 40){
+      System.out.println("Whoa, big pizza!You might need a truck to get this home!");
+    }
+
   }
 }
